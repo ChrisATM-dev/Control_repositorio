e2008cfca81efb019b264665335b69ec4e090197
Christian Torres <Christian.atm24@gmail.com>
2024-06-09 21:34:17 -0400

    nuevo ejemplo

---
+++ Calculadora.py
@@ -0,0 +1,31 @@
+def suma(n1,n2):
+    return 0
+
+def resta(n1,n2):
+    return 0
+
+def multiplicacion(n1,n2):
+    return 0
+
+def division(n1,n2):
+    return 0
+
+n1 = int(input("Numero 1: "))
+n2 = int(input("Numero 2: "))
+while True:
+    opcion = int(input("Que quieres hacer con los numeros?4\n1.-Sumar\n2.-Restar\n3.-Multiplicar\n4.-Dividir\n0.-Salir"))
+
+    if (opcion == 1):
+        suma(n1,n2)
+
+    elif (opcion == 2):
+        suma(n1,n2)
+
+    elif (opcion == 3):
+        suma(n1,n2)
+
+    elif (opcion == 4):
+        suma(n1,n2)
+
+    elif (opcion == 0):
+        break
